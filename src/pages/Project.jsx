e1b5d8196b8a559ba0fb3d916e63371ac01479b0
Royfracis28 <enriquezroyfrancis@gmail.com>
2025-12-12 08:21:30 -0500

    Added new UI design and updated announcements layout

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -24,7 +24,7 @@ export default function Project() {
       <Row>
         <ProjectCard
           title="Cross Platform"
-          github="https://appetize.io/embed/b_clf3yzcnyyg5af25nlkyh7d7ty?device=pixel9pro&launchUrl=exp%3A%2F%2Fu.expo.dev%2F933fd9c0-1666-11e7-afca-d980795c5824%3Fruntime-version%3Dexposdk%253A54.0.0%26channel-name%3Dproduction%26snack%3D%2540loloy28%252Fcrud%26snack-channel%3DgIehylNiNP&params=%7B%22EXDevMenuDisableAutoLaunch%22%3Atrue%2C%22EXKernelDisableNuxDefaultsKey%22%3Atrue%7D&appearance=light&deviceColor=black&scale=auto&orientation=portrait&centered=both"
+          github="https://crossplatform28-git-main-royfrancis-projects.vercel.app/"
         />
         {/* Add more ProjectCard components here */}
       </Row>
